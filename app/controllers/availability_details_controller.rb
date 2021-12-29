ec32b24bf83d11fdb250ca103f2a21d8935c74e0
Rolando Martinez <rolandomtz@hotmail.com>
2021-12-29 02:44:06 -0600

    modificaciones

--- app/controllers/availability_details_controller.rb
+++ app/controllers/availability_details_controller.rb
@@ -1,5 +1,28 @@
 class AvailabilityDetailsController < ApplicationController
+  def prepara_edicion
+    respond_to do |format|
+        format.html { redirect_to editar_disponibilidad_url(params[:availability_id])}
+    end    
+  end
+
   def edit
-    byebug
+    @disponibilidad = Availability.find_by(id: params[:id])
+    @dias_disponibles = AvailabilityDetail.where(availability_id: params[:id]).select("fecha").group("fecha").order(fecha: :asc)
+    @horarios_disponibles = AvailabilityDetail.detalle_horario(params[:id])
+    @modifica = true
+  end
+
+  def update
+    if !params["chkA"].present?
+        return false, "Debe seleccionar al menos un horario"
+    end
+
+    params["chkA"].each do |check|
+        detalle = AvailabilityDetail.find_by(id: check.to_i)
+        detalle.disponible = true
+        detalle.save
+    end
+
+    redirect_to availabilities_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'availability_details/obten_disponibilidad'
   get 'availability_detail/obten_disponibilidad'
-  get '/availabilities', to: "availabilities#index"
+  get '/availabilities', to: "availabilities#index", as: "availabilities"
   post 'semanas_por_servicio', to: "availabilities#semanas_por_servicio", as: "semanas_por_servicio"
   post 'ver_disponibilidad', to: "availabilities#obten_disponibilidad"
-  post 'editar_disponibilidad', to: "availability_details#edit", as: "editar_disponibilidad"
+  post 'editar_disponibilidad', to: "availability_details#prepara_edicion", as: "prepara_editar_disponibilidad"
+  get '/editar_disponibilidad/:id', to: "availability_details#edit", as: "editar_disponibilidad"
+  post '/actualiza_disponibilidad', to: "availability_details#update"
   resources :contracts
   resources :engineer_availabilities
   root to: 'home#index'
